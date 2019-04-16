e24b781a27d322a002479f203c765ba4a6e704cf
zengqihao <zengqihao@videopls.com>
2019-04-16 14:29:57 +0800

    use sass

--- pages/classbinding/index.vue
+++ pages/classbinding/index.vue
@@ -1,34 +1,33 @@
 <template lang="pug">
-  div.classbinding-main-body.transition-container
+  div.classBinding-main-body.transition-container
     h1.h1-class(
       :class="myClass"
     ) Class Binding Component ({{ randomNum }}) {{ reversedMessage }}
     return-btn
 </template>
 
-<style scoped>
-  .classbinding-main-body {
-    position: relative;
-    display: block;
-    margin: 60px auto;
-    width: 100%;
-    max-width: 600px;
-  }
-  .h1-class {
-    font-size: 28px;
-    text-align: center;
-    line-height: 1.7;
-    margin: 35px 0;
-  }
-  .h1-red {
-    color: red;
-  }
-  .h1-bold {
-    font-weight: bold;
-  }
-  .h1-underline {
-    border-bottom: 1px solid red;
-  }
+<style lang="sass" scoped>
+  @import '~@/assets/style/_partial/variables'
+
+  .classBinding-main-body
+    position: relative
+    display: block
+    margin: 60px auto
+    width: 100%
+    max-width: 600px
+
+  .h1
+    &-class
+      font-size: 28px
+      text-align: center
+      line-height: 1.7
+      margin: 35px 0
+    &-red
+      color: $lightSeaGreen
+    &-bold
+      font-weight: bold
+    &-underline
+      border-bottom: 1px solid $turquoise
 </style>
 
 <script>
@@ -36,7 +35,7 @@
 
   export default {
     name: 'ClassBinding',
-    transition: 'slide-left',
+    transition: 'slide-top',
     data() {
       return {
         myClass: ['h1-red', 'h1-bold', 'h1-underline'],
